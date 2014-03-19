bc166c146ae5229af2f72fd5ca73a5b6845a55eb
Ethan S. Chan <eschan@andrew.cmu.edu>
2014-03-19 18:31:35 -0400

    Initial Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WhereToEat::Application.config.secret_key_base = 'a5e5033b6b65b56cb0029494bec2f9a1aa1cafb01939ba4e86c170c88c5b667f4accf43963c7218f73101e206cebeba0f3afbe82243f7328aa7cb7ae387e5c85'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WhereToEat::Application.config.session_store :cookie_store, key: '_WhereToEat_session'
